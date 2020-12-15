0c76ca8823d02d971f84c23f732705f3860bbb06
DonaldLika <donaldlika@hotmail.com>
2020-12-15 23:26:27 +0100

    Update day 1 implementation and tests

--- day-1/index.py
+++ day-1/index.py
@@ -1,3 +1,8 @@
+import unittest
+import os
+
+######################### Solution ###############################
+
 def findFirstCombination(totalNumbers, inputValues, summary=2020):
      if totalNumbers == 1:
         return [summary] if summary in inputValues  else None
@@ -6,3 +11,33 @@ def findFirstCombination(totalNumbers, inputValues, summary=2020):
         result= findFirstCombination(totalNumbers-1,inputValues,summary-n)
         if result:
             return [n,*result]
+
+######################### Test ###############################
+
+testinput = set(map(int, '''\
+1721
+979
+366
+299
+675
+1456'''.splitlines()))
+
+a, b = findFirstCombination(2, testinput)
+assert a+b == 2020, "Should be 2020"
+assert a*b == 514579, "Should be 514579"
+
+a, b, c = findFirstCombination(3, testinput)
+assert a+b+c == 2020, "Should be 2020"
+assert a*b*c == 241861950, "Should be 241861950"
+
+################### Print results for part 1,2 ################
+with open("input") as f:
+         fileInput= set(map(int, f.readlines()))
+
+a, b = findFirstCombination(2, fileInput)
+
+print("2020 with 2 numbers combination {} x {}= {}".format(a,b,a*b))
+
+a, b, c = findFirstCombination(3, fileInput)
+
+print("2020 with 3 numbers combination {} x {} x {} = {}".format(a,b,c,a*b*c))
