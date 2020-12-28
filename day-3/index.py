5d7e4a9a0dc720685bf0e7c46cbb7f7817a7952d
Donald Lika <d.lika@epages.com>
2020-12-28 20:59:41 +0100

    Add exercise 2 solution for day 3

--- day-3/index.py
+++ day-3/index.py
@@ -1,15 +1,25 @@
 from itertools import islice
+from functools import reduce
+from operator import mul
 
-######################### Solution ###############################
-def find_nr_of_trees(road):
+######################### Solution exercise 1 ###############################
+def find_nr_of_trees(road,right=3,down=1):
     col=0
     nrOfTrees=0
-    for row in islice(road.splitlines(), None, None, 1):
+    for row in islice(road.splitlines(), None, None, down):
         currentPosition= row[col % len(row)]
-        col+=3;
+        col+=right;
         if (currentPosition=="#"): nrOfTrees+=1
             
     return nrOfTrees
+
+######################## Solution exercise 2 ###############################
+
+def test_slopes(map):
+    dirs = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+    counts = (find_nr_of_trees(map, r, d) for r, d in dirs)
+    return reduce(mul, counts)
+
 
 ######################### Test ###############################
 
@@ -37,4 +47,13 @@ with open("input") as f:
 
 nrOfTrees= find_nr_of_trees(road)
 
-print("Nr of trees", nrOfTrees)
+print("Nr of trees part 1", nrOfTrees)
+
+########################## Print results for part 2 #################################
+
+with open("input") as f:
+         road = f.read()
+
+nrOfTrees= test_slopes(road)
+
+print("Nr of trees part 2", nrOfTrees)
